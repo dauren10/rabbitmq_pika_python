bcbc8d84d7762518e31f20ec83a3186b43d999ca
Dauren Shalabayev <d.shalabayev@bsl.dev>
2022-10-30 20:24:36 +0600

    Update send

--- send.py
+++ send.py
@@ -1,3 +1,4 @@
+from time import sleep
 import pika
 
 connection = pika.BlockingConnection(
@@ -5,7 +6,8 @@ connection = pika.BlockingConnection(
 channel = connection.channel()
 
 channel.queue_declare(queue='hello')
-
-channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
-print(" [x] Sent 'Hello World!'")
+for i in range(10):
+    channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+    print(" [x] Sent 'Hello World!'")
+    sleep(3)
 connection.close()
