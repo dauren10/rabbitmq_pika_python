bfad68fc16c3ee22ddcc3ece668be044f5356d1c
dauren.shalabayev <dauren.shaalabayev@gmail.com>
2022-11-15 18:03:20 +0600

    Credentials

--- send.py
+++ send.py
@@ -1,8 +1,8 @@
 from time import sleep
 import pika
-
+credentials = pika.PlainCredentials('admin', 'admin')
 connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
+    pika.ConnectionParameters(host='localhost',credentials=credentials))
 channel = connection.channel()
 
 channel.queue_declare(queue='hello')
